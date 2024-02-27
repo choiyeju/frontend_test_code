24f0cc83b87c0192bd196dbeef41d2a944bd52ea
최예주 <yeju.choi@trialinformatics.com>
2024-02-27 16:01:09 +0900

    feat: login form 추가

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,10 +1,30 @@
-
+import "./Login.css";
 
 function Login() {
 
+    const handleChangeId = () => {
+        return;
+    };
+
+    const handleChangePw = () => {
+        return;
+    };
+
     return (
         <div className="Login">
-            Login
+            <form className="login_box">
+                <fieldset>
+                    <legend>로그인</legend>
+                    <p>
+                        <h6>아이디</h6>
+                        <input type="text" id="login_id" onChange={handleChangeId} />
+                    </p>
+                    <p>
+                        <h6>비밀번호</h6>
+                        <input type="password" id="login_pw" onChange={handleChangePw} />
+                    </p>
+                </fieldset>
+            </form>
         </div>
     );
 }
