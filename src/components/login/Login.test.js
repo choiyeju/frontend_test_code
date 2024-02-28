d0e8969be7f9c0c952734f346292e9ea8fbc88dd
최예주 <yeju.choi@trialinformatics.com>
2024-02-28 14:28:33 +0900

    fix: fireEvent, userEvent 사용

--- src/components/login/Login.test.js
+++ src/components/login/Login.test.js
@@ -1,6 +1,6 @@
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Login from './Login';
+import userEvent from "@testing-library/user-event";
 
 test('Login test', () => {
     const onSubmit = jest.fn();
@@ -30,12 +30,33 @@ it('로그인이 가능한가?', () => {
     const login_pw = screen.getByLabelText('비밀번호');
     const submit_button = screen.getByText(/Submit/i);
 
-    userEvent.type(login_id, "remy");
-    userEvent.type(login_pw, "vsecure");
-    userEvent.click(submit_button);
+    fireEvent.change(login_id, { target: { value: "remy" }});
+    fireEvent.change(login_pw, { target: { value: "vsecure" }});
+    fireEvent.click(submit_button);
 
-    // expect(onSubmit).toHaveBeenCalledWith({
-    //     id: "remy",
-    //     pw: "vsecure",
-    // });
+    expect(onSubmit).toHaveBeenCalled();
+    expect(onSubmit).toHaveBeenCalledWith({
+        id: "remy",
+        pw: "vsecure"
+    });
+});
+
+test("userEvent 사용이 가능한가", async () => {
+    const event = userEvent.setup();
+    const onSubmit = jest.fn();
+    render(<Login onSubmit={onSubmit} />);
+
+    const email = screen.getByLabelText("아이디");
+    const password = screen.getByLabelText("비밀번호");
+    const button = screen.getByRole("button");
+
+    await event.type(email, "user@test.com");
+    await event.type(password, "Test1234");
+    await event.click(button);
+
+    expect(onSubmit).toHaveBeenCalledTimes(1);
+    expect(onSubmit).toHaveBeenCalledWith({
+        id: "user@test.com",
+        pw: "Test1234"
+    });
 });
