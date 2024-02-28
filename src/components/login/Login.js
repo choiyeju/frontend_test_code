ca9307129bfe74c464e68a2d45cfd9df274834a6
최예주 <yeju.choi@trialinformatics.com>
2024-02-28 09:47:43 +0900

    feat: login form 추가

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,29 +1,34 @@
 import "./Login.css";
+import {useState} from "react";
 
-function Login() {
+function Login({ onSubmit }) {
+    const [values, setValues] = useState({
+        id: "",
+        pw: ""
+    });
 
-    const handleChangeId = () => {
-        return;
+    const handleChange = (e) => {
+        setValues({
+            ...values,
+            [e.target.id]: e.target.value,
+        });
     };
 
-    const handleChangePw = () => {
-        return;
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onSubmit(values);
     };
 
     return (
         <div className="Login">
-            <form className="login_box">
-                <fieldset>
-                    <legend>로그인</legend>
-                    <p>
-                        <h6>아이디</h6>
-                        <input type="text" id="login_id" onChange={handleChangeId} />
-                    </p>
-                    <p>
-                        <h6>비밀번호</h6>
-                        <input type="password" id="login_pw" onChange={handleChangePw} />
-                    </p>
-                </fieldset>
+            <form className="login_box" onSubmit={handleSubmit}>
+                <label htmlFor="id">아이디</label>
+                <input id="id" type="text" data-testid="login_id" placeholder="아이디" value={values.id} onChange={handleChange}/>
+
+                <label htmlFor="pw">비밀번호</label>
+                <input id="pw" type="password" data-testid="login_pw" placeholder="비밀번호" value={values.pw} onChange={handleChange}/>
+
+                <button type="submit">Submit</button>
             </form>
         </div>
     );
